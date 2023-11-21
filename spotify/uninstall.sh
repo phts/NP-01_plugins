54d48afaf34c07833e0dfe1e99cc58b8292d31ab
Phil Tsaryk <phil@tsaryk.com>
2023-11-21 16:43:27 +0100

    fix(spotify): remove librespot config on plugin uninstall

--- spotify/uninstall.sh
+++ spotify/uninstall.sh
@@ -4,6 +4,7 @@ echo "Unistalling go-librespot-daemon"
 
 rm /usr/bin/go-librespot
 rm /lib/systemd/system/go-librespot-daemon.service
+rm -f /tmp/go-librespot-config.yml
 
 echo "Done"
 echo "pluginuninstallend"
