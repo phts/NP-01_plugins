cd648c0562e1600c51cf165e92cde60fcd41e680
Phil Tsaryk <phil@tsaryk.com>
2024-02-09 16:05:27 +0100

    style(spotify): reformat index.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,13 +4,4 @@ const config = require('@phts/prettier-config');
 module.exports = {
   ...config,
   semi: true,
-  overrides: [
-    {
-      files: 'spotify/index.js',
-      options: {
-        // to avoid a lot of conflicts with original file
-        tabWidth: 4,
-      },
-    },
-  ],
 };
